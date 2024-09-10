ab3670ff08123562b7789a88e2d619d8b93571e8
Usov <usov.lesha2017@gmail.com>
2024-09-10 10:46:55 +0300

    add Validation

---
+++ ProdutsService/Application/Validators/AddListDTOValidator.cs
@@ -0,0 +1,20 @@
+﻿using Application.DTO;
+using FluentValidation;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Application.Validators
+{
+    public class AddListDTOValidator:AbstractValidator<AddListDTO>
+    {
+        public AddListDTOValidator()
+        {
+            RuleFor(p=>p.Name).NotNull().NotEmpty().WithMessage("Name is required");
+            RuleFor(p=>p.Price).NotNull().NotEmpty().GreaterThan(0).WithMessage("Price is required");
+            RuleFor(p=>p.Weight).NotNull().NotEmpty().GreaterThan(0).WithMessage("Weight is required");
+        }
+    }
+}
