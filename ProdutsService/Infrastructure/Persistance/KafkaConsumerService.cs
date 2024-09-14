8a26dea52aa98e41fa461aea0199ea7504667680
Usov <usov.lesha2017@gmail.com>
2024-09-14 20:53:47 +0300

    add Kafka

--- ProdutsService/Infrastructure/Persistance/KafkaConsumerService.cs
+++ ProdutsService/Infrastructure/Persistance/KafkaConsumerService.cs
@@ -1,4 +1,5 @@
 ﻿using Confluent.Kafka;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using System;
@@ -9,63 +10,46 @@ namespace Infrastructure.Persistance
 {
     public class KafkaConsumerService : BackgroundService
     {
-        private readonly IConsumer<Null, string> _consumer;
+        private readonly IConfiguration _configuration;
         private readonly ILogger<KafkaConsumerService> _logger;
 
-        public KafkaConsumerService(ILogger<KafkaConsumerService> logger)
+        public KafkaConsumerService(IConfiguration configuration, ILogger<KafkaConsumerService> logger)
         {
+            _configuration = configuration;
             _logger = logger;
-            var config = new ConsumerConfig
-            {
-                GroupId = "test",
-                BootstrapServers = "localhost:9093",
-                AutoOffsetReset = AutoOffsetReset.Earliest,
-                EnableAutoCommit = true // Автоматическое подтверждение offset
-            };
-
-            _consumer = new ConsumerBuilder<Null, string>(config).Build();
-            _consumer.Subscribe("test");
         }
 
         protected override async Task ExecuteAsync(CancellationToken stoppingToken)
         {
-            _logger.LogInformation("Kafka consumer started");
-
-            try
+            var config = new ConsumerConfig
             {
-                while (!stoppingToken.IsCancellationRequested)
-                {
-                    try
-                    {
-                        var consumeResult = _consumer.Consume(stoppingToken);
+                BootstrapServers = _configuration["Kafka:BootstrapServers"],
+                GroupId = "getProductsInfo-group",
+                AutoOffsetReset = AutoOffsetReset.Earliest
+            };
 
-                        if (consumeResult != null)
-                        {
-                            // Обработка сообщения
-                            _logger.LogInformation($"Received message: {consumeResult.Message.Value}");
-                        }
-                    }
-                    catch (ConsumeException ex)
+            using (var consumer = new ConsumerBuilder<Ignore, string>(config).Build())
+            {
+                consumer.Subscribe(_configuration["Kafka:Topic1"]);
+                consumer.Subscribe(_configuration["Kafka:Topic2"]);
+
+                try
+                {
+                    while (!stoppingToken.IsCancellationRequested)
                     {
-                        _logger.LogError($"Kafka consume error: {ex.Error.Reason}");
+                        var cr = consumer.Consume(stoppingToken);
+                        _logger.LogInformation($"Consumed message '{cr.Value}' from '{cr.Topic}' at: '{cr.Offset}'.");
                     }
                 }
+                catch (ConsumeException e)
+                {
+                    _logger.LogError($"Error occurred: {e.Error.Reason}");
+                }
+                finally
+                {
+                    consumer.Close();
+                }
             }
-            catch (OperationCanceledException)
-            {
-                _logger.LogInformation("Kafka consumer stopping...");
-            }
-            finally
-            {
-                // Корректно завершить работу Consumer
-                _consumer.Close();
-            }
-        }
-
-        public override void Dispose()
-        {
-            _consumer.Dispose();
-            base.Dispose();
         }
     }
 }
