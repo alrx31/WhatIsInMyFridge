8435326f135a7d05827b08399b1513b4db65a8c3
Usov <usov.lesha2017@gmail.com>
2024-09-14 17:44:02 +0300

    add kafka draft

---
+++ FridgeService/DAL/Interfaces/ICacheRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Interfaces
+{
+    public interface ICacheRepository
+    {
+        Task<T?> GetCacheData<T>(string key);
+
+        Task SetCatcheData<T>(string key, T data, TimeSpan? expiry = null);
+
+        Task RemoveCacheData(string key);
+    }
+}
---
+++ FridgeService/DAL/Persistanse/KafkaProducer.cs
@@ -0,0 +1,30 @@
+﻿using Confluent.Kafka;
+using DAL.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DAL.Persistanse
+{
+    public class KafkaProducer: IKafkaProducer
+    {
+        private readonly IProducer<Null, string> _producer;
+
+        public KafkaProducer()
+        {
+            var config = new ProducerConfig
+            {
+                BootstrapServers = "kafka:9092"
+            };
+
+            _producer = new ProducerBuilder<Null, string>(config).Build();
+        }
+
+        public async Task ProduceAsync(string topic, string message)
+        {
+            await _producer.ProduceAsync(topic, new Message<Null, string> { Value = message });
+        }
+    }
+}
