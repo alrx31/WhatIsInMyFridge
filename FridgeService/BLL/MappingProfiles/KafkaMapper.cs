9e24c42f3fc02537bfda81c06f89a5229c507b04
Usov <usov.lesha2017@gmail.com>
2024-09-14 20:45:33 +0300

    add Kafka

--- FridgeService/BLL/MappingProfiles/KafkaMapper.cs
+++ FridgeService/BLL/MappingProfiles/KafkaMapper.cs
@@ -18,7 +18,7 @@ namespace BLL.MappingProfiles
                 .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.Item1))
                 .ForMember(dest => dest.FridgeId, opt => opt.MapFrom(src => src.Item2))
                 .ForMember(dest => dest.Count, opt => opt.MapFrom(src => 0));
-
+            
             CreateMap<(string, int,int), DAL.Entities.MessageBrokerEntities.ProductRemove>()
                 .ForMember(dest => dest.ProductId, opt => opt.MapFrom(src => src.Item1))
                 .ForMember(dest => dest.FridgeId, opt => opt.MapFrom(src => src.Item2))
