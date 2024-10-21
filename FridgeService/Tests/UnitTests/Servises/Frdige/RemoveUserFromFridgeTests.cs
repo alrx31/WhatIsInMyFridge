6f12b74e861161812d32e22cb2b61a9b74c91418
Usov <usov.lesha2017@gmail.com>
2024-10-21 18:14:55 +0300

    implement addusertofridge, removefridge and removeuserfromfridge tests

---
+++ FridgeService/Tests/UnitTests/Servises/Frdige/RemoveUserFromFridgeTests.cs
@@ -0,0 +1,97 @@
+﻿using AutoMapper;
+using BLL.Exceptions;
+using BLL.Services;
+using Bogus;
+using DAL.Entities;
+using DAL.Interfaces;
+using DAL.IRepositories;
+using DAL.Persistanse.Hubs;
+using DAL.Repositories;
+using Microsoft.AspNetCore.SignalR;
+using Microsoft.Extensions.Logging;
+using Moq;
+
+namespace Tests.UnitTests.Servises.Frdige
+{
+    public class RemoveUserFromFridgeTests
+    {
+        private readonly Mock<IFridgeRepository> _fridgeRepository;
+        private readonly Mock<IMapper> _mapper;
+        private readonly Mock<IUnitOfWork> _unitOfWork;
+        private readonly Mock<IgRPCService> _grpcService;
+        private readonly Mock<IProductsgRPCService> _productsgRPCService;
+        private readonly Mock<IKafkaProducer> _kafkaProducer;
+        private readonly Mock<IHubContext<NotificationHub>> _notificationHub;
+        private readonly Mock<ILogger<FridgeService>> _logger;
+
+
+        private readonly IFridgeService _handler;
+
+        public RemoveUserFromFridgeTests()
+        {
+            _mapper = new Mock<IMapper>();
+            _fridgeRepository = new Mock<IFridgeRepository>();
+
+            _unitOfWork = new Mock<IUnitOfWork>();
+
+            _unitOfWork.SetupGet(x => x.FridgeRepository).Returns(_fridgeRepository.Object);
+
+            _grpcService = new Mock<IgRPCService>();
+
+            _kafkaProducer = new Mock<IKafkaProducer>();
+
+            _notificationHub = new Mock<IHubContext<NotificationHub>>();
+
+            _productsgRPCService = new Mock<IProductsgRPCService>();
+
+            _logger = new Mock<ILogger<FridgeService>>();
+
+            _handler = new FridgeService(
+                _fridgeRepository.Object,
+                _mapper.Object,
+                _unitOfWork.Object,
+                _grpcService.Object,
+                _productsgRPCService.Object,
+                _kafkaProducer.Object,
+                _logger.Object,
+                _notificationHub.Object
+            );
+        }
+
+        [Fact]
+        public async Task RemoveUserFromFridge_Success_ShouldRemoveUserFromFridge()
+        {
+            var faker = new Faker();
+            var fridge = new Fridge
+            {
+                id = faker.Random.Number(1, 100),
+                name = faker.Person.FirstName,
+                model = faker.Random.Number(0, 1000000000).ToString(),
+                serial = faker.Random.Number(0, 100000000).ToString(),
+                boughtDate = DateTime.UtcNow,
+                boxNumber = faker.Random.Number(0, 10)
+            };
+            
+            // Act
+
+            await _handler.RemoveUserFromFridge(fridge.id, faker.Random.Number(1, 100));
+
+            // Assert
+
+            _fridgeRepository.Verify(x => x.RemoveUserFromFridge(fridge.id, It.IsAny<int>()), Times.Once);
+            _unitOfWork.Verify(x => x.CompleteAsync(), Times.Once);
+        }
+
+        [Fact]
+        public async Task RemoveUserFromFridge_Fail_WhenFridgeIdOrUserIdInvelid()
+        {
+            // Act
+
+            Func<Task> act = async () => await _handler.RemoveUserFromFridge(0, 0);
+
+            // Assert
+
+            await Assert.ThrowsAsync<BadRequestException>(act);
+        }
+    }
+}
