bc67b15bade2e0a02838f83ca2778d59f75e7bd8
Usov <usov.lesha2017@gmail.com>
2024-10-28 20:57:17 +0300

    add queries tests

---
+++ ProdutsService/Tests/UnitTests/UseCases/Queries/GetAllProductsQueryTests.cs
@@ -0,0 +1,65 @@
+﻿using Application.UseCases.Queries;
+using Application.UseCases.QueriesHandlers;
+using Bogus;
+using Domain.Entities;
+using Domain.Repository;
+using FluentAssertions;
+using Moq;
+
+namespace Tests.UnitTests.UseCases.Queries
+{
+    public class GetAllProductsQueryTests
+    {
+        private readonly Mock<IProductRepository> _productRepository;
+
+        private readonly GetAllProductsQueryHandler _handler;
+
+        public GetAllProductsQueryTests()
+        {
+            _productRepository = new Mock<IProductRepository>();
+
+            _handler = new GetAllProductsQueryHandler(
+                _productRepository.Object
+            );
+        }
+
+        [Fact]
+        public async Task GetAllProductsQuery_Success_ShouldReturnProducts()
+        {
+            var faker = new Faker();
+            var products = new List<Product>
+            {
+                new Product
+                {
+                    Id = faker.Random.Int().ToString(),
+                    Name = faker.Lorem.Word(),
+                    PricePerKilo = faker.Random.Decimal(),
+                    ExpirationTime = new TimeSpan(faker.Random.Int(), faker.Random.Int(), faker.Random.Int()),
+                },
+                new Product
+                {
+                    Id = faker.Random.Int().ToString(),
+                    Name = faker.Lorem.Word(),
+                    PricePerKilo = faker.Random.Decimal(),
+                    ExpirationTime = new TimeSpan(),
+                }
+            };
+
+            var query = new GetAllProductsQuery()
+            {
+                Page = faker.Random.Int(),
+                Count = faker.Random.Int()
+            };
+
+            _productRepository.Setup(x => x.GetAllPaginationAsync(query.Page, query.Count, new CancellationToken())).ReturnsAsync(products);
+
+            // Act
+
+            var result = await _handler.Handle(query, new CancellationToken());
+
+            // Assert
+
+            result.Should().BeEquivalentTo(products);
+        }
+    }
+}
---
+++ ProdutsService/Tests/UnitTests/UseCases/Queries/GetProductQueryTests.cs
@@ -0,0 +1,55 @@
+﻿using Application.UseCases.Queries;
+using Application.UseCases.QueriesHandlers;
+using Bogus;
+using Domain.Entities;
+using Domain.Repository;
+using FluentAssertions;
+using Moq;
+
+namespace Tests.UnitTests.UseCases.Queries
+{
+    public class GetProductQueryTests
+    {
+        private readonly Mock<IProductRepository> _repository;
+
+        private readonly GetProductQueryHandler _handler;
+
+        public GetProductQueryTests()
+        {
+            _repository = new Mock<IProductRepository>();
+
+            _handler = new GetProductQueryHandler(
+                _repository.Object
+            );
+        }
+
+        [Fact]
+        public async Task GetProductQuery_Success_ShouldReturnProduct()
+        {
+            var faker = new Faker();
+            var product = new Product
+            {
+                Id = faker.Random.Guid().ToString(),
+                Name = faker.Random.String(),
+                PricePerKilo = faker.Random.Decimal(),
+                ExpirationTime = new TimeSpan()
+            };
+
+            _repository.Setup(x => x.GetByIdAsync(It.IsAny<string>(), It.IsAny<CancellationToken>()))
+                .ReturnsAsync(product);
+
+            var request = new GetProductQuery
+            (
+                product.Id
+            );
+
+            // Act
+
+            var result = await _handler.Handle(request, CancellationToken.None);
+
+            // Assert
+
+            result.Should().BeEquivalentTo(product);
+        }
+    }
+}
